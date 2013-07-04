3d25a5bbed27206652ab555f06796645167ecc2a
Matt McKegg <matt@wetsand.co.nz>
2013-07-04 00:25:33 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+var Through = require('through')
+
+module.exports = function(){
+
+  var playback = {notes: [], length: 8}
+
+  var onNotes = []
+  var turnOffNotes = []
+
+  var ditty = Through(function(schedule){
+    var events = getRange(schedule.from, schedule.to)
+    events.forEach(function(event){
+      var time = schedule.time + (event.delta*schedule.beatTime)
+      ditty.queue({
+        data: event.data, 
+        time: time, 
+        position: schedule.from+event.delta
+      })
+    })
+  })
+
+  ditty.getNotes = function(){
+    return playback.notes
+  }
+
+  ditty.getLength = function(){
+    return playback.length
+  }
+
+  ditty.clear = function(){
+    ditty.setNotes([])
+  }
+
+  ditty.setNotes = function(notes, length){
+    notes = notes || []
+    turnOffUnused(notes)
+    playback = {notes: notes, length: length || playback.length}
+  }
+
+  ditty.turnOffAllNotes = function(){
+    turnOffUnused()
+  }
+
+  function turnOffUnused(notes){
+    onNotes.forEach(function(note){
+      if (!notes || !notes.some(function(n){ return n.key == note.key })){
+        turnOffNotes.push(note)
+      }
+    })
+  }
+
+  function getRange(start, end){
+    var events = []
+
+    if (turnOffNotes.length){
+      turnOffNotes.forEach(function(note){
+        onNotes[note.key] = null
+        events.push({data: note.off, delta: 0})
+      })
+      turnOffNotes = []
+    }
+
+    playback.notes.forEach(function(note){
+      var position = getAbsolutePosition(note.position, start, playback.length)
+      var endPosition = getAbsolutePosition(note.position + note.length, start, playback.length)
+      if (position>=start && position<end){
+        events.push({data: note.on, delta: position-start})
+        onNotes.push(note)
+      }
+    })
+
+    for (var i=onNotes.length-1;i>=0;i--){
+      var note = onNotes[i]
+      var endPosition = getAbsolutePosition(note.position + note.length, start, playback.length)
+      if (endPosition>=start && endPosition<end){
+        events.push({data: note.off, delta: endPosition-start})
+        onNotes.splice(i, 1)
+      }
+    }
+
+    onNotes.forEach(function(note){
+
+    })
+
+    return events
+  }
+
+
+  return ditty
+}
+
+
+function getAbsolutePosition(pos, start, length){
+  var micro = start % length
+  var position = start+pos-micro
+
+  if (position < start){
+    return position + length
+  } else {
+    return position
+  }
+}
